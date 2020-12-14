ff18d21d62aa6ecbfd72ad3ca449fc4f57866ca2
bojanalukic99 <lukicb99@gmail.com>
2020-12-14 17:20:29 +0100

    #izmene u kodu klase model

--- Studentska_sluzba-OISISI/src/Model/Model.java
+++ Studentska_sluzba-OISISI/src/Model/Model.java
@@ -6,11 +6,14 @@ public class Model {
 	
 	private ArrayList<Ocena> ocene;
 	private ArrayList<Student> studenti;
+	private ArrayList<Profesor> profesori;
+	private ArrayList<Predmet> predmeti;
 	
 	public Model() 
 	{
 		ocene= new ArrayList<Ocena>();
-		
+		profesori= new ArrayList<Profesor>();
+		predmeti= new ArrayList<Predmet>();
 		studenti=new ArrayList<Student>();
 	}
 	
@@ -51,5 +54,113 @@ public class Model {
 		ocene.add(ocena);
 		return true;
 	}
+	
+	public ArrayList<Profesor> getProfesori() {
+		return profesori;
+	}
+	
+	public void setProfesori(ArrayList<Profesor> profesori) {
+		this.profesori = profesori;
+	}
+	public ArrayList<Predmet> getPredmeti() {
+		return predmeti;
+	}
+	public void setPredmeti(ArrayList<Predmet> predmeti) {
+		this.predmeti = predmeti;
+	}
+	
+	public Model(ArrayList<Student> studenti, ArrayList<Profesor> profesori, ArrayList<Predmet> predmeti,
+			ArrayList<Ocena> ocene) {
+		super();
+
+		this.profesori = profesori;
+		this.predmeti = predmeti;
+		
+	}
+	
+	public boolean dodajProfesora(Profesor p)
+	{
+		for(int i=0; i<profesori.size(); i++)
+		{
+			if(profesori.get(i).getBrojLicneKarte()==p.getBrojLicneKarte())
+			{
+				return false;
+			}
+			profesori.add(p);
+		
+		}
+		return true;
+	}
+	
+	public boolean dodajPredmet(Predmet p)
+	{
+		for(int i=0; i<predmeti.size(); i++)
+		{
+			if(predmeti.get(i).getSifraPredmeta()==p.getSifraPredmeta())
+			{
+				return false;
+			}
+			predmeti.add(p);
+		
+		}
+		return true;
+	}
+	
+	public  boolean izbrisiProfesora(Profesor p)
+	{
+		for(int i=0; i<profesori.size(); i++)
+		{	
+			if(profesori.get(i).getBrojLicneKarte()==p.getBrojLicneKarte())
+			{
+				studenti.remove(p);
+				return true;
+			}
+		}
+	return false;
+	}
+	
+	public boolean izbrisiPredmet(Predmet p)
+	{
+		for(int i=0; i<predmeti.size(); i++)
+		{
+			if(predmeti.get(i).getSifraPredmeta()==p.getSifraPredmeta())
+			{
+				predmeti.remove(p);
+				return true;
+			}
+		}
+	return false;
+	}
+	
+	public Profesor nadjiProfesora(String brojLicneKarte )
+	{
+	for(int i=0;i<profesori.size();i++)
+	{
+	if(profesori.get(i).getBrojLicneKarte()==brojLicneKarte)
+	{
+	return profesori.get(i);
+	}
+
+	}
+	return null;
+	}
+	
+	public Predmet nadjiPredmet(String sifraPredmeta)
+	{
+	for(int i=0;i<predmeti.size();i++)
+	{
+	if(predmeti.get(i).getSifraPredmeta()==sifraPredmeta)
+	{
+	return predmeti.get(i);
+
+	}
+
+	}
+	return null;
+	}
+	
+	
+	
+	
 
 }
