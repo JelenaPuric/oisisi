6a5c079b3dbfa35e012ed62b3fbcfd7e2efc68e6
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2020-12-14 13:55:53 +0100

    #dodavanje_menu_bara Implementacija za dodavanje menu bara

--- Studentska_sluzba-OISISI/src/View/MenuBar.java
+++ Studentska_sluzba-OISISI/src/View/MenuBar.java
@@ -1,5 +1,46 @@
 package View;
 
-public class MenuBar {
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
+
+public class MenuBar extends JMenuBar
+{
+	private JMenu fileMenu;
+	private JMenu editMenu;
+	private JMenu helpMenu;
+	
+	public MenuBar()
+	{
+		fileMenu= new JMenu("File");
+		editMenu= new JMenu("Edit");
+		helpMenu= new JMenu("Help");
+		
+		
+		JMenuItem newItem= new JMenuItem();
+		newItem.setText("New");
+		JMenuItem closeItem= new JMenuItem("Close");
+		
+		fileMenu.add(newItem);
+		fileMenu.add(closeItem);
+		
+		JMenuItem editItem= new JMenuItem("Edit");
+		JMenuItem deleteItem= new JMenuItem("Delete");
+		
+		editMenu.add(editItem);
+		editMenu.add(deleteItem);
+		
+		JMenuItem helpItem= new JMenuItem("Help");
+		JMenuItem aboutItem= new JMenuItem("About");
+		
+		helpMenu.add(helpItem);
+		helpMenu.add(aboutItem);
+		add(fileMenu);
+		add(editMenu);
+		add(helpMenu);
+		
+		
+		
+	}
 }
