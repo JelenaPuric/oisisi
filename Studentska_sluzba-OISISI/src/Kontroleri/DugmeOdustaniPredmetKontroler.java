cdb76de85bf40048a4dc15d43943a436bf17c6aa
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2020-12-17 22:24:41 +0100

    #dodavanje_predmeta kontroler za dugme odustani

--- Studentska_sluzba-OISISI/src/Kontroleri/DugmeOdustaniPredmetKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/DugmeOdustaniPredmetKontroler.java
@@ -1,5 +1,23 @@
 package Kontroleri;
 
-public class DugmeOdustaniPredmetKontroler {
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+
+import View.ProzorDodajPredmet;
+
+public class DugmeOdustaniPredmetKontroler extends  AbstractAction{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		ProzorDodajPredmet.getInstance().dispose();
+		
+	}
+
+	public DugmeOdustaniPredmetKontroler()
+	{
+		super("Odustani");
+	}
 }
