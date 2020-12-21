84d192de32fbef75ba3c0c2aea9028440c814f3d
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2020-12-21 23:14:37 +0100

    #brisanje_studenta kontroler za dugme da

--- Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiBrisanjeKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiBrisanjeKontroler.java
@@ -1,5 +1,48 @@
 package Kontroleri;
 
-public class PotvrdiBrisanjeKontroler {
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+
+import View.MyWindow;
+import View.ProzorObrisiEntitet;
+
+public class PotvrdiBrisanjeKontroler extends AbstractAction {
+	
+	public PotvrdiBrisanjeKontroler()
+	{
+	    super("Da");	
+	}
+		
+		  public void actionPerformed(ActionEvent arg0) {
+				
+			  int index;
+			  index=MyWindow.getInstance().getCentralniPanel().getTabbedPane().getSelectedIndex();
+				
+				if(index==0) 
+				{
+				
+					MyWindow.getInstance().getCentralniPanel().getTblStudenti().getSelectedRow();
+					if(index>=0)
+					{
+						MyWindow.getInstance().getCentralniPanel().getDtmStudenti().removeRow(index);
+					}
+					
+				}
+				else if(index==1)
+				{
+					
+				}
+				else 
+				{
+					//todo
+				}
+				
+				ProzorObrisiEntitet.getInstance(" ").dispose();
+				ProzorObrisiEntitet.setInstance(null);
+			}
+		
+	
+
+  
 }
