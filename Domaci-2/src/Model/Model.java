3e91628d0c4b883c5ff7320b8d40a1849e20f240
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2020-12-27 20:27:32 +0100

    #ocena

--- Domaci-2/src/Model/Model.java
+++ Domaci-2/src/Model/Model.java
@@ -7,19 +7,33 @@ import java.util.ArrayList;
 public class Model implements Serializable
 {
 	static final long serialVersionUID = 5546L;
-
+	private ArrayList<Ocena> ocene;
 	private ArrayList<Profesor> profesori;
 	private ArrayList<Predmet> predmeti;
 
 	
 	public Model()
 	{
-
+		ocene= new ArrayList<Ocena>();
 		profesori= new ArrayList<Profesor>();
 		predmeti= new ArrayList<Predmet>();
 	
 	}
 	
+	
+	
+	public ArrayList<Ocena> getOcene() {
+		return ocene;
+	}
+
+
+
+	public void setOcene(ArrayList<Ocena> ocene) {
+		this.ocene = ocene;
+	}
+
+
+
 	public ArrayList<Profesor> getProfesori() {
 		return profesori;
 	}
@@ -33,15 +47,32 @@ public class Model implements Serializable
 		this.predmeti = predmeti;
 	}
 	
-	public Model(ArrayList<Student> studenti, ArrayList<Profesor> profesori, ArrayList<Predmet> predmeti) {
+	public Model(ArrayList<Student> studenti, ArrayList<Profesor> profesori, ArrayList<Predmet> predmeti, ArrayList<Ocena> ocene) {
 		super();
-	
+	    this.ocene= ocene;
 		this.profesori = profesori;
 		this.predmeti = predmeti;
 		
 	}
 	
-	
+	public boolean dodajOcenu(Ocena ocena)
+	{
+		for(int i=0; i<ocene.size(); i++)
+		{
+			if(ocena.getPredmet().getSifraPredmeta()==ocene.get(i).getPredmet().getSifraPredmeta()) 
+			{
+				if(ocena.getStudent().getIndex()==ocene.get(i).getStudent().getIndex()) 
+				{
+					
+					return false;
+				}
+				
+			}
+		}
+		
+		ocene.add(ocena);
+		return true;
+	}
 	
 	public boolean dodajProfesora(Profesor p)
 	{
