508963f500d7d9edac0dab0f2a6006a0adc060a6
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2020-12-27 20:21:59 +0100

    #dodavanje_studenta

--- Domaci-2/src/Kontroleri/DugmeOdustaniStudentKontroler.java
+++ Domaci-2/src/Kontroleri/DugmeOdustaniStudentKontroler.java
@@ -1,5 +1,24 @@
 package Kontroleri;
 
-public class DugmeOdustaniStudentKontroler {
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+
+import View.ProzorDodajStudenta;
+
+
+public class DugmeOdustaniStudentKontroler extends  AbstractAction{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		ProzorDodajStudenta.getInstance().dispose();
+		
+	}
+
+	public DugmeOdustaniStudentKontroler()
+	{
+		super("Odustani");
+	}
 }
