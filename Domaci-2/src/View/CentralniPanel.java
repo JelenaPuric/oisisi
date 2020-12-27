8f4ba3f031cf6f237761a3af552d5888f0c1e790
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2020-12-27 20:02:58 +0100

    #prikaz_studenta

--- Domaci-2/src/View/CentralniPanel.java
+++ Domaci-2/src/View/CentralniPanel.java
@@ -12,26 +12,37 @@ import javax.swing.JScrollPane;
 public class CentralniPanel extends JPanel{
 	
 	private JTabbedPane tabbedPane;
+	private JTable tblStudenti;
 	private JTable tblProfesori;
+	private DefaultTableModel dtmStudenti;
 	private DefaultTableModel dtmProfesori;
 	
 	public CentralniPanel()
 	{
 		this.setLayout(new BorderLayout());
-		
+		JPanel panelStudenti=new JPanel();
 		JPanel panelProfesori= new JPanel();
+		panelStudenti.setLayout(new BorderLayout());
 		panelProfesori.setLayout(new BorderLayout());
+		
+		Object[] data= {"Indeks","Ime","Prezime","Godina studija","Status","Prosek"};
+		dtmStudenti=new DefaultTableModel(data,0);
+		
+		tblStudenti=new JTable(dtmStudenti);
 
 		Object[] data1= {"Ime", "Prezime", "Titula", "Zvanje"};
 		dtmProfesori= new DefaultTableModel(data1,0);
 
 		tblProfesori= new JTable(dtmProfesori);
 		
+		JScrollPane spStudenti=new JScrollPane(tblStudenti);
 		JScrollPane spProfesori= new JScrollPane(tblProfesori);
 		
+		panelStudenti.add(spStudenti,BorderLayout.CENTER);
 		panelProfesori.add(spProfesori,BorderLayout.CENTER);
 		
 		tabbedPane= new JTabbedPane();
+		tabbedPane.add("Studenti",panelStudenti);
 		tabbedPane.add("Profesori",panelProfesori);
 		
 		add(tabbedPane,BorderLayout.CENTER);
@@ -59,6 +70,22 @@ public class CentralniPanel extends JPanel{
 
 	public void setTabbedPane(JTabbedPane tabbedPane) {
 		this.tabbedPane = tabbedPane;
+	}
+
+	public JTable getTblStudenti() {
+		return tblStudenti;
+	}
+
+	public void setTblStudenti(JTable tblStudenti) {
+		this.tblStudenti = tblStudenti;
+	}
+
+	public DefaultTableModel getDtmStudenti() {
+		return dtmStudenti;
+	}
+
+	public void setDtmStudenti(DefaultTableModel dtmStudenti) {
+		this.dtmStudenti = dtmStudenti;
 	}
 
 
