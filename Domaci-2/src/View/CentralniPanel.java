0008986d46088f921b9189b2a5256ee06de7aaae
bojanalukic99 <lukicb99@gmail.com>
2020-12-27 19:49:42 +0100

    #prikaz_profesora

--- Domaci-2/src/View/CentralniPanel.java
+++ Domaci-2/src/View/CentralniPanel.java
@@ -12,18 +12,45 @@ import javax.swing.JScrollPane;
 public class CentralniPanel extends JPanel{
 	
 	private JTabbedPane tabbedPane;
-
-
+	private JTable tblProfesori;
+	private DefaultTableModel dtmProfesori;
 	
 	public CentralniPanel()
 	{
 		this.setLayout(new BorderLayout());
 		
+		JPanel panelProfesori= new JPanel();
+		panelProfesori.setLayout(new BorderLayout());
+
+		Object[] data1= {"Ime", "Prezime", "Titula", "Zvanje"};
+		dtmProfesori= new DefaultTableModel(data1,0);
+
+		tblProfesori= new JTable(dtmProfesori);
 		
-	
-
-
+		JScrollPane spProfesori= new JScrollPane(tblProfesori);
+		
+		panelProfesori.add(spProfesori,BorderLayout.CENTER);
+		
+		tabbedPane= new JTabbedPane();
+		tabbedPane.add("Profesori",panelProfesori);
+		
 		add(tabbedPane,BorderLayout.CENTER);
+	}
+
+	public JTable getTblProfesori() {
+		return tblProfesori;
+	}
+
+	public void setTblProfesori(JTable tblProfesori) {
+		this.tblProfesori = tblProfesori;
+	}
+
+	public DefaultTableModel getDtmProfesori() {
+		return dtmProfesori;
+	}
+
+	public void setDtmProfesori(DefaultTableModel dtmProfesori) {
+		this.dtmProfesori = dtmProfesori;
 	}
 
 	public JTabbedPane getTabbedPane() {
