8f0432d908162d8babdfad44625a15ce77620ad3
bojanalukic99 <lukicb99@gmail.com>
2020-12-29 21:11:57 +0100

    #upis_ocene dodavanje kontrolera za dugme polaganje

---
+++ Studentska_sluzba-OISISI/src/Kontroleri/DugmePolaganjeKontroler.java
@@ -0,0 +1,30 @@
+package Kontroleri;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import Model.Predmet;
+import View.MyWindow;
+import View.ProzorUnosOcene;
+
+public class DugmePolaganjeKontroler extends AbstractAction{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		int indexp= MyWindow.getInstance().getCentralniPanel().getTblPredmeti().getSelectedRow();
+		Predmet p=MyWindow.getInstance().getModel().getPredmeti().get(indexp);
+		ProzorUnosOcene.getInstance().getTxtSifra().setText(p.getSifraPredmeta());
+		ProzorUnosOcene.getInstance().getTxtSifra().setEditable(false);
+		ProzorUnosOcene.getInstance().getTxtSifra().setText(p.getNazivPredmeta());
+		ProzorUnosOcene.getInstance().getTxtNaziv().setEditable(false);
+		ProzorUnosOcene.getInstance().show();
+	}
+
+	public DugmePolaganjeKontroler()
+	{
+		super("Polaganje");
+	}
+}
