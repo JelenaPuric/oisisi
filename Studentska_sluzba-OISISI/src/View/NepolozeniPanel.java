8a5142fdeaff22898c6c2f67c6ed3375d7bc1daa
bojanalukic99 <lukicb99@gmail.com>
2020-12-29 21:05:42 +0100

    #prikaz_nepolozenih_ispita

--- Studentska_sluzba-OISISI/src/View/NepolozeniPanel.java
+++ Studentska_sluzba-OISISI/src/View/NepolozeniPanel.java
@@ -1,7 +1,88 @@
 package View;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class NepolozeniPanel extends JPanel{
 
+public class NepolozeniPanel extends JPanel
+{
+  private JButton btnDodaj;
+  private JButton btnObrisi;
+  private JButton btnPolaganje;
+  private JTable tblNepolozeni;
+  private DefaultTableModel dtmNepolozeni;
+  
+  
+  public NepolozeniPanel()
+  {
+	  this.setLayout(new BorderLayout());
+	  
+	  Object[] data= {"Šifra predmeta", "Naziv predmeta", "ESPB","Godina studija","Semestar"};
+	  dtmNepolozeni=new DefaultTableModel(data,0);
+		
+		tblNepolozeni=new JTable(dtmNepolozeni);
+		JScrollPane spNepolozeni=new JScrollPane(tblNepolozeni);
+		this.add(spNepolozeni,BorderLayout.CENTER);
+		
+	btnDodaj=new JButton("Dodaj");
+	btnObrisi=new JButton("Obriši");
+	btnPolaganje=new JButton("Polaganje");
+	
+	 JPanel levi=new JPanel(); 
+	 levi.setLayout(new FlowLayout(FlowLayout.LEFT));
+	 levi.add(btnDodaj);
+	 levi.add(btnObrisi);
+	 levi.add(btnPolaganje);
+	 add(levi,BorderLayout.NORTH);
+	  
+  }
+
+public JButton getBtnDodaj() {
+	return btnDodaj;
+}
+
+public void setBtnDodaj(JButton btnDodaj) {
+	this.btnDodaj = btnDodaj;
+}
+
+public JButton getBtnObrisi() {
+	return btnObrisi;
+}
+
+public void setBtnObrisi(JButton btnObrisi) {
+	this.btnObrisi = btnObrisi;
+}
+
+public JButton getBtnPolaganje() {
+	return btnPolaganje;
+}
+
+public void setBtnPolaganje(JButton btnPolaganje) {
+	this.btnPolaganje = btnPolaganje;
+}
+
+public JTable getTblNepolozeni() {
+	return tblNepolozeni;
+}
+
+public void setTblNepolozeni(JTable tblNepolozeni) {
+	this.tblNepolozeni = tblNepolozeni;
+}
+
+public DefaultTableModel getDtmNepolozeni() {
+	return dtmNepolozeni;
+}
+
+public void setDtmNepolozeni(DefaultTableModel dtmNepolozeni) {
+	this.dtmNepolozeni = dtmNepolozeni;
+}
+  
+  
+  
 }
