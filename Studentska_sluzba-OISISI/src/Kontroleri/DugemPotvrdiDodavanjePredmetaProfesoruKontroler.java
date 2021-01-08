71ab6819dbea0f100de7f2ac277d6bab132ee2da
bojanalukic99 <lukicb99@gmail.com>
2021-01-08 12:19:47 +0100

    #profesor_predaje_predmete kontroler za dugme potvrdi

---
+++ Studentska_sluzba-OISISI/src/Kontroleri/DugemPotvrdiDodavanjePredmetaProfesoruKontroler.java
@@ -0,0 +1,43 @@
+package Kontroleri;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import Model.Predmet;
+import Model.Profesor;
+import View.MyWindow;
+import View.ProzorDodavanjePredmetaProfesoru;
+import View.ProzorIzmenaProfesora;
+
+public class DugemPotvrdiDodavanjePredmetaProfesoruKontroler extends AbstractAction{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	
+	int[] ind= ProzorDodavanjePredmetaProfesoru.getInstance().getTblPredmeti().getSelectedRows();
+	int indP=MyWindow.getInstance().getCentralniPanel().getTblProfesori().getSelectedRow();
+	Profesor prof=MyWindow.getInstance().getModel().getProfesori().get(indP);
+	
+	for(int i=0; i<ind.length; i++) 
+	{
+		String linija=(String) ProzorDodavanjePredmetaProfesoru.getInstance().getDtmPredmeti().getValueAt(ind[i], 0);
+		String[] deloviLinije=linija.split("\\-");
+		System.out.println(deloviLinije[0]);
+		Predmet p= MyWindow.getInstance().getModel().nadjiPredmet(deloviLinije[0]);
+		prof.getProfNaPredmetima().add(p);
+		Object[] rowData= {p.getSifraPredmeta(),p.getNazivPredmeta(),p.getGodinaStudija(),p.getSemestar()};
+		ProzorIzmenaProfesora.getInstance().getDtmPredmeti().addRow(rowData);
+
+	}
+	
+	ProzorDodavanjePredmetaProfesoru.getInstance().dispose();
+		
+	}
+	public DugemPotvrdiDodavanjePredmetaProfesoruKontroler()
+	{
+		super("Potvdi");
+	}
+
+}
