1935c7ae8516542956319b7cef5d6dd0cf32d1b0
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-10 23:43:53 +0100

    #uklanjanje_predmeta_sa_profesora kontroler

--- Studentska_sluzba-OISISI/src/Kontroleri/UklanjanjePredmetaSaProfesoraKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/UklanjanjePredmetaSaProfesoraKontroler.java
@@ -1,5 +1,26 @@
 package Kontroleri;
 
-public class UklanjanjePredmetaSaProfesoraKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.UklanjanjePredmetaSaProfesora;
+
+
+public class UklanjanjePredmetaSaProfesoraKontroler extends AbstractAction {
+	
+	public UklanjanjePredmetaSaProfesoraKontroler()
+	{
+		super("Ukloni predmet");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		UklanjanjePredmetaSaProfesora.getInstance().show();
+	}
+
+
 
 }
