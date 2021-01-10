d16a6b595368990d62e7a32c340a65fef74f540c
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-10 22:39:41 +0100

    #uklanjanje_predmeta_sa_studenta kontroler za dugme da

--- Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiUklanjanjePredmetaStudentuKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiUklanjanjePredmetaStudentuKontroler.java
@@ -1,5 +1,51 @@
 package Kontroleri;
 
-public class PotvrdiUklanjanjePredmetaStudentuKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import Model.Predmet;
+import Model.Student;
+import View.MyWindow;
+import View.ProzorIzmenaStudenta;
+import View.UklanjanjePredmetaStudentu;
+
+
+public class PotvrdiUklanjanjePredmetaStudentuKontroler extends AbstractAction {
+	
+	public PotvrdiUklanjanjePredmetaStudentuKontroler()
+	{
+		super("Da");
+		
+	}
+	
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		int index=ProzorIzmenaStudenta.getInstance().getNepolozeniPanel().getTblNepolozeni().getSelectedRow();
+		int indStud=MyWindow.getInstance().getCentralniPanel().getTblStudenti().getSelectedRow();
+		Student s= MyWindow.getInstance().getModel().getStudenti().get(indStud);
+		if(index!=-1) 
+		{
+			String sifra=(String) ProzorIzmenaStudenta.getInstance().getNepolozeniPanel().getDtmNepolozeni().getValueAt(index, 0);
+			for(int i=0; i<s.getNepolozeniPredmeti().size(); i++) 
+			{
+				Predmet p= s.getNepolozeniPredmeti().get(i);
+				if(p.getSifraPredmeta().equals(sifra)) 
+				{
+					s.getNepolozeniPredmeti().remove(i);
+					break;
+				}
+			}
+			ProzorIzmenaStudenta.getInstance().getNepolozeniPanel().getDtmNepolozeni().removeRow(index);
+			UklanjanjePredmetaStudentu.getInstance().dispose();
+			
+		}
+	}
+
 
 }
+
