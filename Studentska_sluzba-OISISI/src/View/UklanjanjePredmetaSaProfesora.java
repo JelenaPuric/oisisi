2ea79b51bd9ace93367816568e9e6833d47308b4
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-10 23:39:05 +0100

    #uklanjanje_predmeta_sa_profesora napravljen prozor

--- Studentska_sluzba-OISISI/src/View/UklanjanjePredmetaSaProfesora.java
+++ Studentska_sluzba-OISISI/src/View/UklanjanjePredmetaSaProfesora.java
@@ -1,5 +1,116 @@
 package View;
 
-public class UklanjanjePredmetaSaProfesora {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class UklanjanjePredmetaSaProfesora extends JFrame
+{
+
+	private JLabel lblPoruka;
+	private JButton dugmeDa;
+	private JButton dugmeNe;
+	
+private static UklanjanjePredmetaSaProfesora instance;
+
+public static UklanjanjePredmetaSaProfesora getInstance()
+{
+	  if(instance==null)
+	  {
+		  instance= new UklanjanjePredmetaSaProfesora();
+		  
+	  }
+	  
+	  return instance;
+}
+
+
+public UklanjanjePredmetaSaProfesora()
+{
+	setTitle("Ukloni predmet");
+	setLayout(new BorderLayout());
+	 JPanel mainPanel=new JPanel();
+	 mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
+	 Dimension dim= new Dimension(300,200);
+	lblPoruka=new JLabel("Da li ste sigurni?");
+	lblPoruka.setPreferredSize(dim);
+	dugmeDa=new JButton("Da");
+	dugmeNe=new JButton("Ne");
+	dugmeNe.addActionListener(new ActionListener() 
+	{
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			instance=null;
+			dispose();
+			
+		}
+		
+	});
+	JPanel pnlLblPoruka=new JPanel();
+	JPanel pnlDugme=new JPanel();
+	  pnlLblPoruka.setLayout(new FlowLayout());
+	  pnlLblPoruka.add(lblPoruka);
+	  pnlDugme.setLayout(new FlowLayout());
+	  pnlDugme.add(dugmeDa);
+	  pnlDugme.add(dugmeNe);
+	  mainPanel.add(pnlLblPoruka);
+	  mainPanel.add(pnlDugme);
+	  
+	  
+	  add(mainPanel,BorderLayout.CENTER);
+	
+	
+}
+
+
+
+
+
+public static void setInstance(UklanjanjePredmetaSaProfesora instance) {
+	UklanjanjePredmetaSaProfesora.instance = instance;
+}
+
+
+public JLabel getLblPoruka() {
+	return lblPoruka;
+}
+
+
+public void setLblPoruka(JLabel lblPoruka) {
+	this.lblPoruka = lblPoruka;
+}
+
+
+public JButton getDugmeDa() {
+	return dugmeDa;
+}
+
+
+public void setDugmeDa(JButton dugmeDa) {
+	this.dugmeDa = dugmeDa;
+}
+
+
+public JButton getDugmeNe() {
+	return dugmeNe;
+}
+
+
+public void setDugmeNe(JButton dugmeNe) {
+	this.dugmeNe = dugmeNe;
+}
+
+
+
 
 }
