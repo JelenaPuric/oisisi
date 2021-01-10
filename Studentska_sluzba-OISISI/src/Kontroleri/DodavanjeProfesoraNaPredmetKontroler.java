d6b84d39217a7747d4f41f44929672c913fdaf1b
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-10 23:20:11 +0100

    #dodavanje_profesora_na_predmet kontroler

--- Studentska_sluzba-OISISI/src/Kontroleri/DodavanjeProfesoraNaPredmetKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/DodavanjeProfesoraNaPredmetKontroler.java
@@ -1,5 +1,26 @@
 package Kontroleri;
 
-public class DodavanjeProfesoraNaPredmetKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.DodavanjeProfesoraNaPredmet;
+
+public class DodavanjeProfesoraNaPredmetKontroler extends AbstractAction
+{
+	
+	public DodavanjeProfesoraNaPredmetKontroler()
+	{
+		super("+");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		DodavanjeProfesoraNaPredmet.getInstance().show();
+	}
+
+	
 
 }
