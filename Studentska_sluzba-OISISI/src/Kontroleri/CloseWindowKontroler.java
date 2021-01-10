1392c26c04485daf9f9007222ae8a5b1205a1543
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-11 00:23:08 +0100

    #menu_bar kontroler za close dugme

--- Studentska_sluzba-OISISI/src/Kontroleri/CloseWindowKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/CloseWindowKontroler.java
@@ -1,5 +1,23 @@
 package Kontroleri;
 
-public class CloseWindowKontroler {
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
+
+import View.MyWindow;
+
+public class CloseWindowKontroler extends AbstractAction
+{
+
+	public CloseWindowKontroler()
+	{
+		super("Close");
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		MyWindow.getInstance().dispose();
+	}
+ 
 }
+
