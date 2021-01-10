19c01d4b741afccd208dffd38a6f1a7913466e97
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-10 22:35:34 +0100

    #uklanjanje_predmeta_sa_studenta napravljen kontroler

--- Studentska_sluzba-OISISI/src/Kontroleri/UklanjanjePredmetaStudentuKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/UklanjanjePredmetaStudentuKontroler.java
@@ -1,5 +1,25 @@
 package Kontroleri;
 
-public class UklanjanjePredmetaStudentuKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.UklanjanjePredmetaStudentu;
+
+public class UklanjanjePredmetaStudentuKontroler extends AbstractAction {
+	
+	public UklanjanjePredmetaStudentuKontroler()
+	{
+		super("Ukloni");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		UklanjanjePredmetaStudentu.getInstance().show();
+	}
+
+
 
 }
