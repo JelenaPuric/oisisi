812576bbdf9153c8f92b5132efaa447654049417
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-10 22:23:08 +0100

    #dodavanje_predmeta_studentu kontroler za potvrdu dodavanja

--- Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiDodavanjePredmetaStudentuKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiDodavanjePredmetaStudentuKontroler.java
@@ -1,5 +1,40 @@
 package Kontroleri;
 
-public class PotvrdiDodavanjePredmetaStudentuKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import Model.Predmet;
+import Model.Student;
+import View.DodavanjePredmetaStudentu;
+import View.MyWindow;
+
+public class PotvrdiDodavanjePredmetaStudentuKontroler extends AbstractAction
+{
+   public PotvrdiDodavanjePredmetaStudentuKontroler()
+   {
+	   super("Dodaj");
+   }
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		int[] indexi= DodavanjePredmetaStudentu.getInstance().getTblPredmeti().getSelectedRows();
+		int row=MyWindow.getInstance().getCentralniPanel().getTblStudenti().getSelectedRow();
+		Student stud=MyWindow.getInstance().getModel().getStudenti().get(row);
+		for(int i=0; i<indexi.length; i++) 
+		{
+			System.out.println("usao "+indexi[i]);
+			String linija=(String) DodavanjePredmetaStudentu.getInstance().getDtmPredmeti().getValueAt(indexi[i], 0);
+			String[] deloviLinije=linija.split("\\-");
+			Predmet p= MyWindow.getInstance().getModel().nadjiPredmet(deloviLinije[0]);
+			
+			
+		}
+		DodavanjePredmetaStudentu.getInstance().dispose();
+		
+		
+	}
 
 }
