bfada4676743c8b496f7d175a9ee9523cbbbd86b
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-11 00:00:21 +0100

    #uklanjanje_predmeta_sa_profesora

--- Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiUklanjanjePredmetaSaProfesoraKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiUklanjanjePredmetaSaProfesoraKontroler.java
@@ -1,5 +1,50 @@
 package Kontroleri;
 
-public class PotvrdiUklanjanjePredmetaSaProfesoraKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import Model.Predmet;
+import Model.Profesor;
+import View.MyWindow;
+import View.ProzorIzmenaProfesora;
+import View.UklanjanjePredmetaSaProfesora;
+
+
+public class PotvrdiUklanjanjePredmetaSaProfesoraKontroler extends AbstractAction {
+	
+	public PotvrdiUklanjanjePredmetaSaProfesoraKontroler()
+	{
+		super("Potvrdi");
+		
+	}
+	
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		int index=ProzorIzmenaProfesora.getInstance().getTblPredmeti().getSelectedRow();
+		int indProf=MyWindow.getInstance().getCentralniPanel().getTblProfesori().getSelectedRow();
+		Profesor p= MyWindow.getInstance().getModel().getProfesori().get(indProf);
+		if(index!=-1) 
+		{
+			String sifra=(String) ProzorIzmenaProfesora.getInstance().getDtmPredmeti().getValueAt(index, 0);
+			for(int i=0; i<p.getProfNaPredmetima().size(); i++) 
+			{
+				Predmet pr= p.getProfNaPredmetima().get(i);
+				if(pr.getSifraPredmeta().equals(sifra)) 
+				{
+					p.getProfNaPredmetima().remove(i);
+					break;
+				}
+			}
+			ProzorIzmenaProfesora.getInstance().getDtmPredmeti().removeRow(index);
+			UklanjanjePredmetaSaProfesora.getInstance().dispose();
+			
+		}
+	}
+
 
 }
