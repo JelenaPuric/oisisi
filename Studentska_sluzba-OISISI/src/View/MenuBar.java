a68dbe3980400aebaa7d943cd7e568a68c609cc3
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-11 00:18:19 +0100

    #menu_bar dodati aksceleratori, mneumonici i importovan about dijalog

--- Studentska_sluzba-OISISI/src/View/MenuBar.java
+++ Studentska_sluzba-OISISI/src/View/MenuBar.java
@@ -1,8 +1,17 @@
 package View;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import Kontroleri.AboutKontroler;
+import Kontroleri.IzmenaEntitetKontroler;
+import Kontroleri.ProzorDodajEntitetKontroler;
+import Kontroleri.ProzorObrisiKontroler;
+
 
 
 public class MenuBar extends JMenuBar
@@ -18,21 +27,57 @@ public class MenuBar extends JMenuBar
 		helpMenu= new JMenu("Help");
 		
 		
-		JMenuItem newItem= new JMenuItem();
+		JMenuItem newItem= new JMenuItem(new ProzorDodajEntitetKontroler());
 		newItem.setText("New");
+		  KeyStroke keyStrokeToOpen
+		    = KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK);
+		 newItem.setAccelerator(keyStrokeToOpen);
+		 
+		 
 		JMenuItem closeItem= new JMenuItem("Close");
+		
+		  KeyStroke keyStrokeToClose
+		    = KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK);
+		 closeItem.setAccelerator(keyStrokeToClose);
+	
 		
 		fileMenu.add(newItem);
 		fileMenu.add(closeItem);
 		
-		JMenuItem editItem= new JMenuItem("Edit");
-		JMenuItem deleteItem= new JMenuItem("Delete");
+		JMenuItem editItem= new JMenuItem(new IzmenaEntitetKontroler());
+		editItem.setText("Edit");
+		  KeyStroke keyStrokeToEdit
+		    = KeyStroke.getKeyStroke(KeyEvent.VK_E, KeyEvent.CTRL_DOWN_MASK);
+		 editItem.setAccelerator(keyStrokeToEdit);
+		
+		JMenuItem deleteItem= new JMenuItem(new ProzorObrisiKontroler());
+		deleteItem.setText("Delete");
+		  KeyStroke keyStrokeToDelete
+		    = KeyStroke.getKeyStroke(KeyEvent.VK_D, KeyEvent.CTRL_DOWN_MASK);
+		 deleteItem.setAccelerator(keyStrokeToDelete);
 		
 		editMenu.add(editItem);
 		editMenu.add(deleteItem);
 		
 		JMenuItem helpItem= new JMenuItem("Help");
-		JMenuItem aboutItem= new JMenuItem("About");
+		helpItem.setText("Help");
+		  KeyStroke keyStrokeToHelp
+		    = KeyStroke.getKeyStroke(KeyEvent.VK_H, KeyEvent.CTRL_DOWN_MASK);
+		 helpItem.setAccelerator(keyStrokeToHelp);
+		 
+		JMenuItem aboutItem= new JMenuItem(new AboutKontroler());
+		aboutItem.setText("About");
+		  KeyStroke keyStrokeToAbout
+		    = KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK);
+		 aboutItem.setAccelerator(keyStrokeToAbout);
+		 
+		 //mneumonici
+		 newItem.setMnemonic(KeyEvent.VK_N);
+		 closeItem.setMnemonic(KeyEvent.VK_C);
+		 editItem.setMnemonic(KeyEvent.VK_E);
+		 deleteItem.setMnemonic(KeyEvent.VK_D);
+		 helpItem.setMnemonic(KeyEvent.VK_H);
+		 aboutItem.setMnemonic(KeyEvent.VK_A);
 		
 		helpMenu.add(helpItem);
 		helpMenu.add(aboutItem);
