3c781fd4dc2c1d64b634734c04c08af6765e8c85
bojanalukic99 <lukicb99@gmail.com>
2021-01-11 18:10:58 +0100

    #greska

--- Studentska_sluzba-OISISI/src/File/ReadWrite.java
+++ Studentska_sluzba-OISISI/src/File/ReadWrite.java
@@ -12,49 +12,8 @@ import View.MyWindow;
 
 
 public class ReadWrite {
-	
-	public static void citajFile(String putanja)
-	{
-		Model model;
-		try {
-			FileInputStream fis= new FileInputStream(putanja);
-			ObjectInputStream obj=new ObjectInputStream(fis);
-			model=(Model)obj.readObject();
-			MyWindow.getInstance().setModel(model);
-			fis.close();
-			obj.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-	}
-	
-	public static void pisiFile(String putanja)
-	{
-	
-		try {
-			FileOutputStream fos = new FileOutputStream(putanja);
-			ObjectOutputStream obj= new ObjectOutputStream(fos);
-			obj.writeObject(MyWindow.getInstance().getModel());
-			fos.close();
-			obj.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+
+
 
 }
