5df1e07d9029aeba469e5d5676ea103bc0435447
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-12 09:20:26 +0100

    #ponistavanje_ocene kontroler za prikaz prozora

--- Studentska_sluzba-OISISI/src/Kontroleri/PonistavanjeOceneKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PonistavanjeOceneKontroler.java
@@ -1,5 +1,27 @@
 package Kontroleri;
 
-public class PonistavanjeOceneKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.PonistavanjeOcene;
+
+public class PonistavanjeOceneKontroler extends AbstractAction {
+	
+	public PonistavanjeOceneKontroler()
+	{
+		super("Poništi ocenu");
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		PonistavanjeOcene.getInstance().show();
+		
+	}
+
+	
 
 }
