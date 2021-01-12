49bbacdd75137204e8a9254dab260798b445bea9
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-12 09:26:23 +0100

    #ponistavanje_ocene kontroler za dugme da

--- Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiPonistavanjeOceneKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiPonistavanjeOceneKontroler.java
@@ -1,5 +1,33 @@
 package Kontroleri;
 
-public class PotvrdiPonistavanjeOceneKontroler {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import Model.Student;
+import View.MyWindow;
+import View.PonistavanjeOcene;
+import View.ProzorIzmenaStudenta;
+
+public class PotvrdiPonistavanjeOceneKontroler extends AbstractAction
+{
+	
+	public PotvrdiPonistavanjeOceneKontroler()
+	{
+		super("Da");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		int index= ProzorIzmenaStudenta.getInstance().getPolozeniPanel().getTblPolozeni().getSelectedRow();
+		int indStud=MyWindow.getInstance().getCentralniPanel().getTblStudenti().getSelectedRow();
+		Student s= MyWindow.getInstance().getModel().getStudenti().get(indStud);
+		s.getOcjene().remove(index);
+		PonistavanjeOcene.getInstance().dispose();
+	}
+
+	
 
 }
+
