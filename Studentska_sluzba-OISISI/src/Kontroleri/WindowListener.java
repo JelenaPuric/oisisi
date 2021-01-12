c90eeb9596653bc6d3cfa72ce6fb8edf8d4bd80c
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-12 09:37:22 +0100

    #deserijalizacija

--- Studentska_sluzba-OISISI/src/Kontroleri/WindowListener.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/WindowListener.java
@@ -7,6 +7,14 @@ import File.ReadWrite;
 import View.CentralniPanel;
 
 public class WindowListener extends WindowAdapter{
+
+	public void windowOpened(WindowEvent e) 
+	{
+		
+		ReadWrite.citajFile("C:\\Users\\ljubi\\Documents\\GitHub\\oisisi\\Studentska_sluzba-OISISI\\src\\Data\\data.ss");
+		
+		
+	}
 	
 	
 	public void windowClosing(WindowEvent e)
