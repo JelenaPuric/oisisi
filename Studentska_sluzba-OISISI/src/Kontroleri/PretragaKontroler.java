e6e190526acb9ece35a61ff672c3e3350ec2270a
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-12 09:10:59 +0100

    #pretraga_studenata implementiran dio koda za pretragu studenata

--- Studentska_sluzba-OISISI/src/Kontroleri/PretragaKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PretragaKontroler.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
 
 import Model.Predmet;
 import Model.Profesor;
+import Model.Student;
 import View.MyWindow;
 import View.ProzorDodajProfesora;
 
@@ -21,6 +22,7 @@ public class PretragaKontroler extends AbstractAction {
 		String text= MyWindow.getInstance().getToolBar().getPoljePretrazi().getText();
 		ArrayList<Profesor> pretrazeniProfesori= new ArrayList<Profesor>();
 		ArrayList<Predmet> pretrazeniPredmeti= new ArrayList<Predmet>();
+		ArrayList<Student> pretrazeniStudenti= new ArrayList<Student>();
 		if(text.equals("")) 
 		{
 			JOptionPane.showMessageDialog(null, "Morate popuniti tekstualno polje za pretragu!!!");
@@ -30,8 +32,60 @@ public class PretragaKontroler extends AbstractAction {
 		String[] deloviLinije= text.split(" ");
 		if(ind==0)
 		{
-			//to do
+			if(deloviLinije.length==1)
+			{
+				String deoPrezimena=deloviLinije[0];
+				for(int i=0; i<MyWindow.getInstance().getModel().getStudenti().size(); i++)
+				{
+					Student st=MyWindow.getInstance().getModel().getStudenti().get(i);
+					if(st.getPrezime().toLowerCase().contains(deoPrezimena.toLowerCase()))
+					{
+						pretrazeniStudenti.add(st);
+					}
+				}
+				
+				popuniPretrazeneStudente(pretrazeniStudenti);
+			}
+			
+			else if(deloviLinije.length==2)
+			{
+				String deoPrezimena=deloviLinije[0];
+				String deoImena=deloviLinije[1];
+				for(int i=0; i<MyWindow.getInstance().getModel().getStudenti().size(); i++)
+				{
+					Student st=MyWindow.getInstance().getModel().getStudenti().get(i);
+					if(st.getIme().toLowerCase().contains(deoImena.toLowerCase()) && st.getPrezime().toLowerCase().contains(deoPrezimena.toLowerCase()) )
+					{
+						pretrazeniStudenti.add(st);
+					}
+				}
+				
+				popuniPretrazeneStudente(pretrazeniStudenti);
+				
+			}
+			
+			else if(deloviLinije.length==3)
+			{
+				String deoPrezimena=deloviLinije[0];
+				String deoImena=deloviLinije[1];
+				String deoIndexa=deloviLinije[2];
+				for(int i=0; i<MyWindow.getInstance().getModel().getStudenti().size(); i++)
+				{
+					Student st=MyWindow.getInstance().getModel().getStudenti().get(i);
+					if(st.getIndex().toLowerCase().contains(deoIndexa.toLowerCase()) && st.getPrezime().toLowerCase().contains(deoPrezimena.toLowerCase()) && st.getIme().toLowerCase().contains(deoImena.toLowerCase()) )
+					{
+						pretrazeniStudenti.add(st);
+					}
+				}
+				
+				popuniPretrazeneStudente(pretrazeniStudenti);
+			}
+			
+			
+				
+			
 		}
+		
 		else if(ind==1)
 		{
 			if(deloviLinije.length==1)
@@ -95,6 +149,19 @@ public class PretragaKontroler extends AbstractAction {
 	{
 		super("Pretraga");
 	}
+	
+	public void popuniPretrazeneStudente(ArrayList<Student> studenti)
+	{
+		MyWindow.getInstance().getCentralniPanel().getDtmStudenti().setRowCount(0);
+		for(int i=0; i<studenti.size(); i++) 
+		{
+			Student s=studenti.get(i);
+			Object[] data= {s.getIndex(),s.getIme(),s.getPrezime(),s.getGodina(),s.getStatus(),s.getProsjecnaOcjena()};
+			MyWindow.getInstance().getCentralniPanel().getDtmStudenti().addRow(data);
+			
+		}
+	}
+	
 	
 	public void popuniPretrazeneProfesore(ArrayList<Profesor> profesori) 
 	{
