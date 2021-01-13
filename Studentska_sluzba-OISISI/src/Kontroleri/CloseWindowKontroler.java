61d0fdb63b0bf6a98445b19912ed616a7b2f484a
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-13 17:38:08 +0100

    #menu_bar dodavanje ikonica

--- Studentska_sluzba-OISISI/src/Kontroleri/CloseWindowKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/CloseWindowKontroler.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
+import File.Configuartion;
 import View.MyWindow;
 
 public class CloseWindowKontroler extends AbstractAction
@@ -11,7 +12,7 @@ public class CloseWindowKontroler extends AbstractAction
 
 	public CloseWindowKontroler()
 	{
-		super("Close");
+		super("Close",Configuartion.closeIcon);
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
