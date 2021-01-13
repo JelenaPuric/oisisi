771dcd7f061477d06a44c58de82c95d1b8c30711
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-13 21:24:04 +0100

    #brisanje_profesora

--- Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiBrisanjeKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/PotvrdiBrisanjeKontroler.java
@@ -33,11 +33,11 @@ public class PotvrdiBrisanjeKontroler extends AbstractAction {
 				}
 				else if(index==1)
 				{
-					int i=MyWindow.getInstance().getCentralniPanel().getTblProfesori().getSelectedRow();
-					if(index>=0)
+					int ind=MyWindow.getInstance().getCentralniPanel().getTblProfesori().getSelectedRow();
+					if(ind>=0)
 					{
-						MyWindow.getInstance().getCentralniPanel().getDtmProfesori().removeRow(index);
-						MyWindow.getInstance().getModel().getProfesori().remove(i);
+						MyWindow.getInstance().getCentralniPanel().getDtmProfesori().removeRow(ind);
+						MyWindow.getInstance().getModel().getProfesori().remove(ind);
 
 					}
 				}
