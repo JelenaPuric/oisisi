ecb3aa0a3457ab0d683a762b3e0f15cc27f89177
bojanalukic99 <lukicb99@gmail.com>
2021-01-13 22:13:39 +0100

    #glavni_prozor

--- DOMACI-1/src/View/CentralniPanel.java
+++ DOMACI-1/src/View/CentralniPanel.java
@@ -1,4 +1,4 @@
-
+package View;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
--- DOMACI-1/src/View/MyWindow.java
+++ DOMACI-1/src/View/MyWindow.java
@@ -7,11 +7,15 @@ import javax.swing.JFrame;
 
 
 
+
 public class MyWindow extends JFrame
 
 {
  
   private StatusBar statusBar;
+  private ToolBar toolBar;
+  private CentralniPanel centralniPanel;
+  
 
   private static MyWindow instance;
   
@@ -29,13 +33,31 @@ public class MyWindow extends JFrame
   public MyWindow()
   {
 	  this.setLayout(new BorderLayout());
-	  
+	  centralniPanel= new CentralniPanel();
+		add(centralniPanel,BorderLayout.CENTER);
 	  setJMenuBar(new MenuBar());
 	  statusBar= new StatusBar();
 	  add(statusBar,BorderLayout.SOUTH);
 	 
 	  }
+  
 
+
+public ToolBar getToolBar() {
+	return toolBar;
+}
+
+public void setToolBar(ToolBar toolBar) {
+	this.toolBar = toolBar;
+}
+
+public CentralniPanel getCentralniPanel() {
+	return centralniPanel;
+}
+
+public void setCentralniPanel(CentralniPanel centralniPanel) {
+	this.centralniPanel = centralniPanel;
+}
 
 public StatusBar getStatusBar() {
 	return statusBar;
