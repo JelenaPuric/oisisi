a8f9a389e91c35aa6de214046cbcabbd813fbe72
bojanalukic99 <lukicb99@gmail.com>
2021-01-13 18:57:45 +0100

    #nesto

--- Studentska_sluzba-OISISI/src/Kontroleri/DugmeOdustaniDodavanjePredmetaProfesoruKontroler.java
+++ Studentska_sluzba-OISISI/src/Kontroleri/DugmeOdustaniDodavanjePredmetaProfesoruKontroler.java
@@ -8,16 +8,6 @@ import View.ProzorDodavanjePredmetaProfesoru;
 
 public class DugmeOdustaniDodavanjePredmetaProfesoruKontroler extends AbstractAction {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		ProzorDodavanjePredmetaProfesoru.getInstance().dispose();
-	}
 	
-	public DugmeOdustaniDodavanjePredmetaProfesoruKontroler()
-	{
-		super("Odustani");
-	}
-
 }
 
