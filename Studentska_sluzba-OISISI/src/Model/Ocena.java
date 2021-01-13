b8a17223dc652f7de9dc10083e8deb3d804b8ba3
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-13 18:16:36 +0100

    #ocena

--- Studentska_sluzba-OISISI/src/Model/Ocena.java
+++ Studentska_sluzba-OISISI/src/Model/Ocena.java
@@ -1,1 +1,52 @@
+package Model;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
+public class Ocena implements Serializable {
+     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1307981810800486554L;
+	private Student student;
+     private Predmet predmet;
+     private int vrijednostOcjene;
+     private LocalDateTime datumPolaganjaPredmeta;
+     
+     public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public Predmet getPredmet() {
+		return predmet;
+	}
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+	public int getVrijednostOcjene() {
+		return vrijednostOcjene;
+	}
+	public void setVrijednostOcjene(int vrijednostOcjene)
+	{
+		if(vrijednostOcjene>=6 && vrijednostOcjene<=10)
+		{
+		this.vrijednostOcjene = vrijednostOcjene;
+	    }
+	}
+	public LocalDateTime getDatumPolaganjaPredmeta() {
+		return datumPolaganjaPredmeta;
+	}
+	public void setDatumPolaganjaPredmeta(LocalDateTime datumPolaganjaPredmeta) {
+		this.datumPolaganjaPredmeta = datumPolaganjaPredmeta;
+	}
+	public Ocena(Student student, Predmet predmet, int vrijednostOcjene, LocalDateTime datumPolaganjaPredmeta) {
+		super();
+		this.student = student;
+		this.predmet = predmet;
+		this.vrijednostOcjene = vrijednostOcjene;
+		this.datumPolaganjaPredmeta = datumPolaganjaPredmeta;
+	}
+	
+
+}
