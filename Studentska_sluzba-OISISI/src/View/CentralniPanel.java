9d9c60fa8e9b1b074c8a7ad1753be010232ba600
bojanalukic99 <lukicb99@gmail.com>
2021-01-13 18:47:31 +0100

    #sortiranje_profesora

--- Studentska_sluzba-OISISI/src/View/CentralniPanel.java
+++ Studentska_sluzba-OISISI/src/View/CentralniPanel.java
@@ -51,7 +51,7 @@ public class CentralniPanel extends JPanel{
 		
 		
 		tblStudenti.setAutoCreateRowSorter(true);
-		
+		tblProfesori.setAutoCreateRowSorter(true);
 		
 		
 		JScrollPane spStudenti= new JScrollPane(tblStudenti);
