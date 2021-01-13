6da4c55933aaf8e5cd11b60fd2f6a75b9b0c13a4
Jelena <73442066+JelenaPuric@users.noreply.github.com>
2021-01-13 18:33:56 +0100

    #status_bar

--- Studentska_sluzba-OISISI/src/View/StatusBar.java
+++ Studentska_sluzba-OISISI/src/View/StatusBar.java
@@ -1,1 +1,56 @@
 
+package View;
+
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class StatusBar extends JPanel
+{
+ private JLabel naziv;
+ private JLabel datum;
+ 
+ public StatusBar()
+ {
+	 naziv= new JLabel("Studentska služba");
+	 datum= new JLabel("Datum");
+	 setLayout(new BorderLayout());
+	 Timer timer=new Timer(1000,new ActionListener()
+	 {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0)
+		{
+			// TODO Auto-generated method stub
+			LocalDateTime date=LocalDateTime.now();
+			String d;
+			d=String.valueOf(date.getHour())+":"+String.valueOf(date.getMinute())+" "+String.valueOf(date.getDayOfMonth())+"."+String.valueOf(date.getMonthValue())+"."+String.valueOf(date.getYear())+".";
+			
+			datum.setText(d);
+			
+		}
+		
+	 });
+	 
+	 timer.start();
+	 JPanel levi= new JPanel();
+	 JPanel desni= new JPanel();
+	 levi.setLayout(new FlowLayout(FlowLayout.LEFT));
+	 desni.setLayout(new FlowLayout(FlowLayout.RIGHT));
+	 levi.add(naziv);
+	 desni.add(datum);
+	 add(levi,BorderLayout.WEST);
+	 add(desni,BorderLayout.EAST);
+	 
+ }
+ 
+ 
+ 
+}
